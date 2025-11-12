e3cbc1214b0262f54f1cf851760cc897c874fdf0
Abenezer Girma <michaelbeki99@gmail.com>
2025-11-12 12:37:21 -0800

    Test hue-rotate at 140deg

--- ark-care-afh/src/app/page.tsx
+++ ark-care-afh/src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Home() {
                       width={400}
                       height={400}
                       className="w-full h-full object-cover"
-                      style={{filter: 'hue-rotate(270deg)'}}
+                      style={{filter: 'hue-rotate(140deg)'}}
                     />
                   </div>
                 </div>
@@ -232,7 +232,7 @@ export default function Home() {
                         width={400}
                         height={400}
                         className="w-full h-full object-cover"
-                        style={{filter: 'hue-rotate(270deg)'}}
+                        style={{filter: 'hue-rotate(140deg)'}}
                       />
                     </div>
                   </div>
